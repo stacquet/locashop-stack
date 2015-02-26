3047041d6da5fd8867c472d122eba12491c7a0f5
unknown <sylvain.tacquet@gmail.com>
2015-02-26 22:43:46 +0100

    modified:   test/selenium/testLogin.js

--- test/selenium/testLogin.js
+++ test/selenium/testLogin.js
@@ -16,7 +16,7 @@ describe('angularjs homepage', function() {
 				element(by.id('homeLink')).click();
 			}
 		});
-		browser.sleep(1000);
+		//browser.sleep(1000);
 	});
     
 	it('click on login should lead to login page', function() {
@@ -25,34 +25,37 @@ describe('angularjs homepage', function() {
 	});
 	it('enter an wrong login email should let login button disabled', function() {
 		loginLink.click();
-		element(by.model('user.email')).sendKeys('sylvain.tacquetgmail.com');
+		element(by.model('vm.user.email')).sendKeys('sylvain.tacquetgmail.com');
 		expect(element(by.id('loginButton')).isEnabled()).toBe(false);
 	});
 	
 	it('enter a valid login and password should let login button enabled', function() {
 		loginLink.click();
-		element(by.model('user.email')).clear();
-		element(by.model('user.email')).sendKeys('sylvain.tacquet@gmail.com');
-		element(by.model('user.password')).sendKeys('tagisy6');
+		element(by.model('vm.user.email')).clear();
+		element(by.model('vm.user.password')).clear();
+		element(by.model('vm.user.email')).sendKeys('sylvain.tacquet@gmail.com');
+		element(by.model('vm.user.password')).sendKeys('tagisy6');
 		expect(element(by.id('loginButton')).getWebElement().isEnabled()).toBe(true);
 	});
 	
 	it('enter a valid login and password and click login should autenticate and lead to home page', function() {
 		loginLink.click();
-		element(by.model('user.email')).clear();
-		element(by.model('user.email')).sendKeys('sylvain.tacquet@gmail.com');
-		element(by.model('user.password')).sendKeys('tagisy6');
+		element(by.model('vm.user.email')).clear();
+		element(by.model('vm.user.password')).clear();
+		element(by.model('vm.user.email')).sendKeys('sylvain.tacquet@gmail.com');
+		element(by.model('vm.user.password')).sendKeys('tagisy6');
 		element(by.id('loginButton')).click();
 		expect(browser.getCurrentUrl()).toEqual(baseUrl+'/');
 	});
 
 	it('enter a wrong login and password and click login should show error message', function() {
 		loginLink.click();
-		element(by.model('user.email')).clear();
-		element(by.model('user.email')).sendKeys('sylvain.tacquet@gmail.com');
-		element(by.model('user.password')).sendKeys('azerazrgfd');
+		element(by.model('vm.user.email')).clear();
+		element(by.model('vm.user.password')).clear();
+		element(by.model('vm.user.email')).sendKeys('sylvain.tacquet@gmail.com');
+		element(by.model('vm.user.password')).sendKeys('azerazrgfd');
 		element(by.id('loginButton')).click();
-		expect(element.all(by.repeater('loginMessage in loginMessages')).count()).toEqual(1);
+		expect(element.all(by.repeater('message in vm.messages')).count()).toEqual(1);
 	});
 
 });
